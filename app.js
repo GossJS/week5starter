e27cc7eb4ec3f054aa2c12f7840ad486705d0e78
Ilya Goss <goss@corp.ifmo.ru>
2020-11-25 01:03:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-export default (express, bodyParser, fs, crypto, http) => {
+export default (express, bodyParser, createReadStream, crypto, http) => {
     const app = express();
 
-    app
-    .get('/login/', (req, res) => res.send('eliasgoss'))
-    .get('/code/', (req, res) => fs.createReadStream(import.meta.url.substring(7)).pipe(res));
-   
-    // другие маршруты
 
-    // проверяющий алгоритм может запустить этот код в своей песочнице
-    // передав ему express и fs
-    // и сравнить то, что выдаёт маршрут /login/ работающего приложения
-    // по предоставляемому обучающимся адресу
-    // с тем, что выдаёт по этому же маршруту вот этот код в песочнице
+    const CORS = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+        'Access-Control-Allow-Headers':'x-test,Content-Type,Accept, Access-Control-Allow-Headers'
+        }; 
+    
+
+    app
+    .use((r, res, next) => { r.res.set(CORS); next(); })
+    .use(bodyParser.urlencoded({ extended: true }))
+    .get('/sha1/:input', r => {
+    
+    })
+    
+    .get('/login/', (req, res) => res.send('goss'))
+    .get('/code/', (req, res) => {
+ 
+    })
+    ;
+
+    app.all('/req/', (req, res) => {
+         
+    })
+ 
+     
     return app;
 
-}
+};
